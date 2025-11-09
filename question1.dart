2548c716e8a4cd04095f97e5cee35f42ef565e6b
Gazi Muhammad Mobasher <143348243+AD3OR@users.noreply.github.com>
2025-11-10 02:25:41 +0600

    done q1

--- question1.dart
+++ question1.dart
@@ -3,16 +3,17 @@
 
 // 1. Create variables of different data types: String, int, double, bool
 // TODO: Add your variables here
-String name = ""; // TODO: Initialize with your name
-int age = 0; // TODO: Initialize with your age
-double height = 0.0; // TODO: Initialize with your height
-bool isStudent = false; // TODO: Initialize with true or false
+String name = "Gazi"; // TODO: Initialize with your name
+int age = 24; // TODO: Initialize with your age
+double height = 5.8; // TODO: Initialize with your height
+bool isStudent = true; // TODO: Initialize with true or false
 
 // 2. Write a function called calculateBMI that takes weight (double) and height (double) as parameters and returns the BMI as a double
 // TODO: Implement the calculateBMI function
 double calculateBMI(double weight, double height) {
   // TODO: Calculate BMI = weight / (height * height)
-  return 0.0; // TODO: Replace with actual calculation
+  double bmi = weight / (height * height);
+  return bmi; // TODO: Replace with actual calculation
 }
 
 // 3. Write a function called getGrade that takes a score (int) and returns a grade (String) based on:
@@ -24,7 +25,18 @@ double calculateBMI(double weight, double height) {
 // TODO: Implement the getGrade function
 String getGrade(int score) {
   // TODO: Add your logic here
-  return ""; // TODO: Return appropriate grade
+  if (score >= 90 && score <= 100) {
+    return "A";
+  } else if (score >= 80 && score < 90) {
+    return "B";
+  } else if (score >= 70 && score < 80) {
+    return "C";
+  } else if (score >= 60 && score < 70) {
+    return "D";
+  } else {
+    return "F";
+  }
+   // TODO: Return appropriate grade
 }
 
 void main() {
